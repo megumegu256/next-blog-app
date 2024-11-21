8a09d6be7810fe8b3a4ba05c963348f2f2a25389
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-11-21 09:37:25 +0900

    update PostSummary.tsx

--- src/app/_components/PostSummary.tsx
+++ src/app/_components/PostSummary.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { Post } from "@/app/_types/Post";
+import Link from "next/link";
 
 type Props = {
   post: Post;
@@ -7,9 +8,17 @@ type Props = {
 
 const PostSummary: React.FC<Props> = (props) => {
   const { post } = props;
+  const safeHTML = post.content;
   return (
     <div className="border border-slate-400 p-3">
       <div className="font-bold">{post.title}</div>
+      <Link href={`/posts/${post.id}`}>
+        <div className="mb-1 text-lg font-bold">{post.title}</div>
+        <div
+          className="line-clamp-3"
+          dangerouslySetInnerHTML={{ __html: safeHTML }}
+        />
+      </Link>
       <div dangerouslySetInnerHTML={{ __html: post.content }} />
     </div>
   );
