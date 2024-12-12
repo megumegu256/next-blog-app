5d0da5fc759f039d923b7343da5de913776d492c
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-12-12 09:24:31 +0900

    build(deps): update next.config.mjs, package-lock.json, package.json and PostSummary.tsx

--- src/app/_components/PostSummary.tsx
+++ src/app/_components/PostSummary.tsx
@@ -11,15 +11,23 @@ const PostSummary: React.FC<Props> = (props) => {
   const safeHTML = post.content;
   return (
     <div className="border border-slate-400 p-3">
-      <div className="font-bold">{post.title}</div>
       <Link href={`/posts/${post.id}`}>
         <div className="mb-1 text-lg font-bold">{post.title}</div>
+        <div className="text-slate-500">
+          {new Date(post.createdAt).toLocaleDateString("ja-JP", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </div>
+        <div className="text-slate-500">{post.categories[0].name}</div>
         <div
           className="line-clamp-3"
           dangerouslySetInnerHTML={{ __html: safeHTML }}
         />
       </Link>
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
     </div>
   );
 };
